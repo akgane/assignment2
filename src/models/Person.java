689352d7aa0146813f1bbc208f6769c648a543e3
akgane <orazgaliahan.af@gmail.com>
2023-12-25 09:45:08 +0600

    Message

---
+++ src/models/Person.java
@@ -0,0 +1,39 @@
+package models;
+
+public abstract class Person implements Payable{
+    static int idCounter = 1;
+    int id;
+    String name, surname;
+
+    public Person(){
+        id = idCounter++;
+        name = "Name";
+        surname = "Surname";
+    }
+    public Person(String name, String surname){
+        id = idCounter++;
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setSurname(String surname){
+        this.surname = surname;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getSurname(){
+        return surname;
+    }
+
+    @Override public String toString(){
+        return id + ". " + name + " " + surname;
+    }
+    public abstract String getPosition();
+}
